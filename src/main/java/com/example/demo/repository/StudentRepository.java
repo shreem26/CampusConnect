14858664c5cccfcc805eeeacf3941b6023012c3c
Chandhini03 <chandhini06sv@gmail.com>
2025-10-31 16:33:26 +0530

    update

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student,Integer> {
