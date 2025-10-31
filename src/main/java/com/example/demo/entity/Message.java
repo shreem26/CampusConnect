e8c05cb57889d6b4e8cca7fa68e5349532c08b84
Chandhini03 <chandhini06sv@gmail.com>
2025-10-31 15:45:52 +0530

    add entity

--- src/main/java/com/example/demo/entity/Message.java
+++ src/main/java/com/example/demo/entity/Message.java
@@ -2,6 +2,8 @@ package com.example.demo.entity;
 import java.time.*;
 import jakarta.persistence.*;
 
+@Entity
+@Table(name = "message")
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
