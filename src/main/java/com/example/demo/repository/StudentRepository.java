91c308568dd43a384a2bc783a632e7bb836e060d
Chandhini03 <chandhini06sv@gmail.com>
2025-11-04 20:28:17 +0530

    .

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -5,13 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.*;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-    Optional<Student> findByName(String name);
-
-    // find student by email
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
     Optional<Student> findByEmail(String email);
 
     // find student by verification token
