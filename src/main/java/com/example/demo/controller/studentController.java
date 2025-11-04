cf6cb21afe51a9c228363235c22ed249ac254f0c
Chandhini03 <chandhini06sv@gmail.com>
2025-11-04 20:22:30 +0530

    .

--- src/main/java/com/example/demo/controller/studentController.java
+++ src/main/java/com/example/demo/controller/studentController.java
@@ -1,4 +1,9 @@
 package com.example.demo.controller;
+
+import com.example.demo.entity.Student;
+import com.example.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import java.util.*;
 public class studentController {
 }
